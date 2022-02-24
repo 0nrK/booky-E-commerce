a747dff86b35f306912147368a2546a41d8d6c49
0nrK <onurkirnapci@gmail.com>
2022-02-24 23:17:53 +0300

    add backend

--- client/src/services/authHeader.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-    const user = JSON.parse(localStorage.getItem("user"))
-
-    if (user && user.accessToken) {
-        return { "x-access-token": user.accessToken }
-    } else {
-        return {}
-    }
-}
